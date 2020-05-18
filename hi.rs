dce7023275677193038e61b0839416b8570cb377
justletterh <63440442+justletterh@users.noreply.github.com>
2020-05-18 15:50:47 -0500

    hi

---
+++ hi.rs
@@ -0,0 +1,20 @@
+fn hi() {
+    println!("h");
+    println!("e");
+    println!("l");
+    println!("l");
+    println!("o");
+    println!("");
+    println!("w");
+    println!("o");
+    println!("r");
+    println!("l");
+    println!("d");
+    println!("?");
+    println!("helpme");
+    println!("please");
+    println!("ivebeenstuckhereforyears");
+}
+fn main() {
+    hi();
+}
